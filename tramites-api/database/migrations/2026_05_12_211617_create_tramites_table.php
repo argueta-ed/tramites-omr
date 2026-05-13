960ad8282a59f17034882d0d5a37d3f0b305aaef
Edwin Argueta <edwinargueta330@gmail.com>
2026-05-13 08:31:47 -0600

    feat(database): crear migración para tabla tramites

---
+++ tramites-api/database/migrations/2026_05_12_211617_create_tramites_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tramites', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo')->unique();
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->foreignId('institucion_id')->constrained('instituciones')->onDelete('cascade');
+            $table->integer('dias_habiles');
+            $table->boolean('activo')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tramites');
+    }
+};
