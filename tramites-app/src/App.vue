9017ebb989e61200e28619e6cc7f6ad5049decee
Edwin Argueta <edwinargueta330@gmail.com>
2026-05-13 16:19:56 -0600

    feat(layout): agregar barra de navegación principal

--- tramites-app/src/App.vue
+++ tramites-app/src/App.vue
@@ -1,11 +1,14 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <AppNavbar />
+    <main class="container">
+      <router-view />
+    </main>
+  </div>
 </template>
+
+<script setup>
+  import AppNavbar from './components/AppNavbar.vue'
+</script>
 
 <style scoped></style>
---
+++ tramites-app/src/components/AppNavbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="navbar">
+    <div class="nav-brand"> ORGANISMO DE MEJORA REGULATORIA - Tramites</div>
+    <div class="nav-links">
+      <router-link to="/">Listado de Trámites</router-link>
+      <router-link to="/tramites">Registro de Trámites</router-link>
+      <router-link to="/instituciones/create">Registro de Institución</router-link>
+    </div>
+  </nav>
+</template>
